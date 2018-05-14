b5a1573a374432f1943c5ae0a120feea0632f2ce
ciaramcneil25 <38012857+ciaramcneil25@users.noreply.github.com>
2018-05-14 15:19:28 -0400

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,125 @@
+// //#QuestiionOne
+//function printInt(n){
+	//for(var i=1;i<=n;i++);
+	//console.log(i);
+//}
+
+// //#QuestionTwo: 
+// function printIntRev(n){
+// 	for(var i = 10; i>=1; i--){
+// 	console.log(i);
+// 	}
+// }
+
+//QuestionThree
+//function checkInput(x){
+	//if (typeof x == "number"){
+	//	return "number"
+	//} else if (typeof x == "string"){
+		//return "string";
+	//} else if (typeof x == "boolean"){
+		//return "boolean";
+	//} else {
+		//return -1;
+	//}
+//}
+
+//console.log(checkInput("hvgyu"))
+
+//Questionfour
+//function simpleEvenAdding(num){
+	//var result = 0;
+	//for var (i = 1; i<=num;i++){
+
+	//}
+	//	if (i % 2 == 0) {
+	//		result += i;
+	//	}
+	//	return result;
+//console.log(simpleEvenAdding(5))
+//}
+
+//Questionfive
+//function letterCapitalize(str){
+//	var splittedArray = str.split(" ")
+//	return splittedArray
+	//for (var i=0;i<splittedArray.length;i++){
+	//	newString += " " + splittedArray[i][0].toUpperCase() + splittedarray[i].slice(1)
+
+//}
+//return "";
+//}
+
+//QuestionSix
+//function reverseString(str){
+	//var myArray = str.split(" ").reverse();
+	//var newString = "";
+	//for(var i=0; i< myArray.length;i++){
+		//newstring += myArray[i];
+	//}
+//	return newstr;
+
+//QuestionSeven
+//function Math.max{
+	//var max = 0
+	//var min = 999999999;
+	//for( var i=0;i<arr.length;i++){
+		//if(arr[i]>max){
+			//max = arr[i]
+		//}
+
+		//if (arr[i]<min){
+		//	min=arr[i];
+		//}
+	//}
+	//console.log(min);
+	//console.log(max);
+	//}
+
+//Questioneight
+//function timeConvert(num) {
+//var h = parseInt(num / 60);
+//var m = num % 60;
+  //return h + ':' + m;
+//}
+
+
+//Questionnine
+
+// findStr(“hih”, “hihellohiho”)=> 3
+
+//function findStr(str,long){
+
+	//var accumulator = 0;
+
+	//for(var i = 0; i <= long.length - str.length;i++){
+			//if(long.slice(i,str.length+i) == str) {
+				//accumulator++;
+			}
+	//}
+
+	//return accumulator;
+//}
+
+//findStr("hi", "dasdhidasdahidashi");
+//findStr("yo", "yololopoyoplopkoyo");
+
+
+//QuiestionTen
+//function selfDividingNumbers(left, right){
+	//var myArr = [];
+	//for(var i = left;i<=right;i++){
+		//check if i is self dividing number
+		//if(isSelfDividing(i)){
+		//	myArr.push(i);
+//		}
+//	}
+
+	//return myArr;
+//}
+
+//is given number self dividing number ?
+//return true if the given number is self dividing
+//return false if the given number is not self dividing
+//function isSelfDividing(num){
+//}
